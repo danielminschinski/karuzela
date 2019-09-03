1614b6da1100cc81008c7f9e4e78890a423dcbde
Daniel Minschinski <danielminschinski@gmail.com>
2019-09-03 21:20:30 +0200

    Add button restart

--- js/script.js
+++ js/script.js
@@ -5,6 +5,7 @@ var flkty = new Flickity( elem, {
   contain: true,
   pageDots: false,
   hash: true,
+
   
 
 });
@@ -13,6 +14,16 @@ var flkty = new Flickity( elem, {
 //   for an individual element
 var flkty = new Flickity( '.main-carousel', {
   // options
- 
+var buttonGroup = document.querySelector('.button-group');
+var buttons = buttonGroup.querySelectorAll('.button');
+buttons = fizzyUIUtils.makeArray( buttons );
+
+buttonGroup.addEventListener( 'click', function( event ) {
+  // filter for button clicks
+  if ( !matchesSelector( event.target, '.button' ) ) {
+    return;
+  }
+  var index = buttons.indexOf( event.target );
+  flkty.select( index );
 
 });
