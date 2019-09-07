9b24b5a42a205421172cc2031786de74fa023ab8
Daniel Minschinski <danielminschinski@gmail.com>
2019-09-07 17:56:15 +0200

    Update progress bar

--- js/script.js
+++ js/script.js
@@ -9,8 +9,9 @@ var flkty = new Flickity( elem, {
 
 // element argument can be a selector string
 //   for an individual element
-var flkty = new Flickity( '.main-carousel', {
+var flkty = new Flickity('.main-carousel')
   // options
+
 var buttonGroup = document.querySelector('.button-group');
 var buttons = buttonGroup.querySelectorAll('.button');
 buttons = fizzyUIUtils.makeArray( buttons );
@@ -22,11 +23,12 @@ buttonGroup.addEventListener( 'click', function( event ) {
   }
   var index = buttons.indexOf( event.target );
   flkty.select( index );
+});
 
-  var progressBar = document.querySelector('.progress-bar')
+var progressBar = document.querySelector('.progress-bar')
 
 flkty.on( 'scroll', function( progress ) {
   progress = Math.max( 0, Math.min( 1, progress ) );
   progressBar.style.width = progress * 100 + '%';
-
 });
+  
