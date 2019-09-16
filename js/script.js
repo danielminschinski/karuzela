26bc4b4974d6903b9c2855dd385b6ed6735d7b5d
Daniel Minschinski <danielminschinski@gmail.com>
2019-09-16 21:37:49 +0200

    Update variable imageData and add loop in script.js

--- js/script.js
+++ js/script.js
@@ -51,11 +51,17 @@ $carousel.on( 'scroll.flickity', function( event, progress ) {
 });
 
 window.initMap = function() {
-  // The location of Uluru
-  var uluru = {lat: -25.344, lng: 131.036};
-  // The map, centered at Uluru
+
+  var gdansk = imageData[0].coords;
   var map = new google.maps.Map(
-      document.getElementById('map'), {zoom: 4, center: uluru});
-  // The marker, positioned at Uluru
-  var marker = new google.maps.Marker({position: uluru, map: map});
-}
+  	document.getElementById('map'), {zoom: 4, center: gdansk});
+
+  for(var i = 0; i < imageData.length; i++){
+  	if(key = 'coords'){
+  		var location = imageData[i].coords;
+  		var marker = new google.maps.Marker({position: location, map: map});
+  	}
+  }
+  
+  
+};
